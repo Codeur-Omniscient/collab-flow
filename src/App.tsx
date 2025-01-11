10b286f373cd3c55e60d2653af0521870a46bbff
Codeur-Omniscient <Anthonymedin258@outlook.fr>
2025-01-11 03:21:23 +0100

    chore:updated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
+import Dashboard from "./pages/Dashboard";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="login" element={<LogIn />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
