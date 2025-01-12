a8cb3cb99d9c78722a9d9cc02481a9a34c37aad4
Codeur-Omniscient <Anthonymedin258@outlook.fr>
2025-01-12 19:57:52 +0100

    style(ui): mise en place des layout du formulaire de connexion

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,5 +1,34 @@
+import SigninForm from "@/features/auth/login/SigninForm";
+import logo from "../assets/logo.svg";
+import signinBanner from "../assets/signin-banner.webp";
+
 const LogIn = () => {
-  return <div>login</div>;
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src={signinBanner}
+          alt="Sign up banner"
+          className="absolute inset-0 h-full w-full object-cover "
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <div className="flex items-center gap-2 font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <img src={logo} alt="logo" />
+            </div>
+            CollabFlow
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SigninForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
