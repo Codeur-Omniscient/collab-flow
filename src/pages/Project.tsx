7bf0cdb5fe720b31ecf73a7a6996e5f407c438c9
Codeur-Omniscient <Anthonymedin258@outlook.fr>
2025-01-18 00:56:59 +0100

    style(pages): refactor Project component to use ProjectDetails and create a new Projects page with ProjectList and ProjectForm

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,31 +1,7 @@
-import { Button } from "@/components/ui/button";
-import ProjectForm from "@/features/projects/ProjectForm";
-import { useProjectStore } from "@/stores/projectStore";
+import ProjectDetails from "@/features/projects/ProjectDetails";
 
 const Project = () => {
-  const { formOpen, isOpen } = useProjectStore();
-  console.log(formOpen);
-  return (
-    <div className="space-y-5 p-4">
-      <div className="flex items-center justify-between gap-4">
-        <div>
-          <h1 className="text-2xl font-semibold">Project</h1>
-          <p className="text-muted">Manage your project here</p>
-        </div>
-        <div>
-          <Button type="button" onClick={isOpen}>
-            Create Project
-          </Button>
-        </div>
-      </div>
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-      </div>
-      <ProjectForm />
-    </div>
-  );
+  return <ProjectDetails />;
 };
 
 export default Project;
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import ProjectForm from "@/features/projects/ProjectForm";
+import ProjectList from "@/features/projects/ProjectList";
+import { useProjectStore } from "@/stores/projectStore";
+
+const Projects = () => {
+  const { formOpen, isOpen } = useProjectStore();
+  console.log(formOpen);
+  return (
+    <div className="space-y-5 p-4">
+      <div className="flex items-center justify-between gap-4">
+        <div>
+          <h1 className="text-2xl font-semibold">Project</h1>
+          <p className="text-muted">Manage your project here</p>
+        </div>
+        <div>
+          <Button type="button" onClick={isOpen}>
+            Create Project
+          </Button>
+        </div>
+      </div>
+      <ProjectList />
+      {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+      </div> */}
+      <ProjectForm />
+    </div>
+  );
+};
+
+export default Projects;
