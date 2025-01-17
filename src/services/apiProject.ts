e7262d9f7f72469afb7241ceed59bfb26724ad06
Codeur-Omniscient <Anthonymedin258@outlook.fr>
2025-01-18 00:55:49 +0100

    feat(api): renommer getProjectApi en getAllProjectApi et ajouter une fonction pour récupérer un projet par ID

--- src/services/apiProject.ts
+++ src/services/apiProject.ts
@@ -1,12 +1,23 @@
 import { Project } from "@/types";
 import supabase from "./supabase";
 
-export async function getProjectApi() {
+export async function getAllProjectApi() {
   const { data: projects, error } = await supabase.from("projects").select("*");
 
   if (error) throw new Error("Failed to get projects");
 
-  return { projects };
+  return projects;
+}
+
+export async function getProjectApi(id: string) {
+  const { data: project, error } = await supabase
+    .from("projects")
+    .select("*")
+    .eq("id", id)
+    .single();
+  if (error) throw new Error(error.message);
+
+  return project;
 }
 
 export async function createProjectApi(project: Project) {
