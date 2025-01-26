f7931aadc2b94ff9c73bee2a98d30a9623534c0f
Codeur-Omniscient <Anthonymedin258@outlook.fr>
2025-01-26 15:44:18 +0100

    feat(store): ajouter un état pour gérer le dialogue de suppression dans le store de formulaire

--- src/stores/formStore.ts
+++ src/stores/formStore.ts
@@ -3,17 +3,21 @@ import { create } from "zustand";
 interface FormStore {
   formOpen: boolean;
   editForm: boolean;
+  deleteDialog: boolean;
   isOpen: () => void;
   onClose: () => void;
   isEditOpen: () => void;
   onEditClose: () => void;
+  onDeleteClose: () => void;
 }
 
 export const useFormStore = create<FormStore>()((set) => ({
   formOpen: false,
   editForm: false,
+  deleteDialog: false,
   isOpen: () => set({ formOpen: true }),
   onClose: () => set({ formOpen: false }),
   isEditOpen: () => set({ editForm: true }),
   onEditClose: () => set({ editForm: false }),
+  onDeleteClose: () => set({ deleteDialog: false }),
 }));
