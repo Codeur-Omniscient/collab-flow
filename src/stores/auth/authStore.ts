ee3391819e8c63fd5e4a37e9502ae4599a1e6660
Codeur-Omniscient <Anthonymedin258@outlook.fr>
2025-01-26 15:42:51 +0100

    feat(auth): ajouter un état d'authentification dans le store d'authentification

--- src/stores/auth/authStore.ts
+++ src/stores/auth/authStore.ts
@@ -3,10 +3,14 @@ import { User } from "@/types";
 
 interface AuthStore {
   user: User | null;
+  isAuthenticated: boolean;
   setUser: (user: User) => void;
+  setIsAuthenticated: (isAuthenticated: boolean) => void;
 }
 
 export const useAuthStore = create<AuthStore>((set) => ({
   user: null,
+  isAuthenticated: false,
   setUser: (user) => set({ user }),
+  setIsAuthenticated: (isAuthenticated) => set({ isAuthenticated }),
 }));
