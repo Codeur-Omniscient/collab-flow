8ffccd092f30c0856ed5bf45fb4fc8187638de78
Codeur-Omniscient <Anthonymedin258@outlook.fr>
2025-01-26 16:57:00 +0100

    feat(nav-user): refactor NavUser component to use auth store and simplify props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,18 +11,18 @@ const Header = () => {
     avatar: "/avatars/shadcn.jpg",
   };
   return (
-    <header className="flex h-16 shrink-0 border-b items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <header className="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
       </div>
-      <div className="flex items-center justify-between gap-2 grow">
+      <div className="flex grow items-center justify-between gap-2">
         <p className="">Welcome back,{USER.name}</p>
         <div className="flex items-center gap-2">
           <Button variant="ghost">
             <Bell />
           </Button>
-          <NavUser user={USER} />
+          <NavUser />
         </div>
       </div>
     </header>
